39ac6311c0f9f3215a90a9c1445234014d49c14b
sergio-florentin-itti <sergio.florentin@itti.digital>
2025-05-27 00:02:32 -0300

    remove image from login form

--- apps/web/src/app/pages/login/index.tsx
+++ apps/web/src/app/pages/login/index.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "./_components/login-form"
 export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-svh items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="w-full max-w-md">
         <LoginForm />
       </div>
     </div>
